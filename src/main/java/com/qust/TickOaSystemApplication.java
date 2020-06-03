2b78584ccfeb80b345eae876fbf6d7fc7d892d81
MrWangShawn <1739809101@qq.com>
2020-06-03 14:39:03 +0800

    滴答项目

---
+++ src/main/java/com/qust/TickOaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.qust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickOaSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TickOaSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qust/TickOaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickOaSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
